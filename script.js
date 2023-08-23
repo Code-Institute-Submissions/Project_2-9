239a82df83409a81077c5e6198ee58db763f2265
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-08-23 15:36:53 +0000

    add all text nodes for the game story

--- script.js
+++ script.js
@@ -86,7 +86,6 @@ Game.prototype.selectOption = function (option) {
 
 
 // Text Nodes starting here
-
 const textNodes = [
 
     // Story Forward
@@ -128,6 +127,158 @@ const textNodes = [
         {
           text: "Unsure. I am scared.",
           nextText: 601,
+        },
+      ],
+    },
+    {
+      id: 4,
+      text: "The Uranium Emperor is already expecting you.",
+      img: "./assets/imperator3.png",
+      options: [
+        {
+          text: "Looking deep into the eyes of the Uranium Emperor.",
+          nextText: 5,
+        },
+        {
+          text: "Better to go home.",
+          nextText: 602,
+        },
+      ],
+    },
+    {
+      id: 5,
+      text: "The final battle to save humanity is at hand. You look deep into the eyes of the Uranium Emperor. You will rescue the world. You just have to want to.",
+      img: "./assets/p_woman4.png",
+      options: [
+        {
+          text: "Take out your sword!",
+          nextText: 6, 
+        },
+        {
+          text: "Unsure. Better watch Netflix.", // Link to GANE OVER Screen
+          nextText: 666,
+        },
+      ],
+    },
+    {
+      id: 6,
+      text: "You hold your sword in your hand and you will defeat the Uranium Emperor. You are powerful. But still be careful. The Emperor is very clever...",
+      img: "./assets/sword.png",
+      options: [
+        {
+          text: "Attack!",
+          nextText: 7,
+        },
+        {
+          text: "Dodging.",
+          nextText: 603,
+        },
+      ],
+    },
+    {
+      id: 7,
+      text: "Great stuff! You hit the Uranium Emperor hard.",
+      img: "./assets/attack1.png",
+      options: [
+        {
+          text: "Attack!",
+          nextText: 8,
+        },
+        {
+          text: "Dodging.",
+          nextText: 604,
+        },
+      ],
+    },
+    {
+      id: 8,
+      text: "Best attack ever! The Uranium Emperor will lose!",
+      img: "./assets/attack2.png",
+      options: [
+        {
+          text: "One last hit.",
+          nextText: 9,
+        },
+      ],
+    },
+    {
+      id: 9,
+      text: "You have triumphed. The Uranium Emperor is now defeated. You are unkillable. Have you noticed that the only way to be GAME OVER is to start watching Netflix?",
+      img: "./assets/won.png",
+      options: [
+        {
+          text: "Play again.",
+          nextText: 1,
+        },
+      ],
+    },
+  
+    // GANE OVER Screen
+    {
+      id: 666,
+      text: "GAME OVER! You cannot escape the Uranium Emperor. You can only defeat him.",
+      img: "./assets/game_over.png",
+      options: [
+        {
+          text: "Try again.",
+          nextText: 1,
+        },
+      ],
+    },
+  
+    // Story Backward
+    {
+      id: 600,
+      text: "You are a super heroine. You are not scared. Jump and save the world!",
+      img: "./assets/not_jump.png",
+      options: [
+        {
+          text: "Go and move forward. It's your job!",
+          nextText: 3,
+        },
+      ],
+    },
+    {
+      id: 601,
+      text: "Phosphorus Woman is in her secret underground cave. Her decision is made. The entire world must be rescued.",
+      img: "./assets/cave.png",
+      options: [
+        {
+          text: "Go and move forward. It's your job!",
+          nextText: 4,
+        },
+      ],
+    },
+    {
+      id: 602,
+      text: "Being at home in the cave does not keep you happy. You have to save the world.",
+      img: "./assets/underground.png",
+      options: [
+        {
+          text: "Go and move forward. It's your job!",
+          nextText: 5,
+        },
+      ],
+    },
+    {
+      id: 603,
+      text: "Perfect. The Uranium Emperor could not hit you.",
+      img: "./assets/dodging1.png",
+      options: [
+        {
+          text: "Attack!",
+          nextText: 7,
+        },
+      ],
+    },
+    {
+      id: 604,
+      text: "Perfect. The Uranium Emperor had no chance to hit you.",
+      img: "./assets/dodging2.png",
+      options: [
+        {
+          text: "Attack!",
+          nextText: 8,
         },
       ],
     },
