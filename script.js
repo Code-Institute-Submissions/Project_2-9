546c0835ddfc223fb4de960356f9c00e860941b8
renebaumann3000 Baumann <rene.baumann84@outlook.com>
2023-08-23 12:32:17 +0000

    Create an instance of the Game class

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initialize the game
+Game.prototype.initialize = function () {
+    // Start the game by displaying the first text node
+    this.showTextNode(1);
+  };
+
+  // Create an instance of the Game class
+var game = new Game();
+// Initialize the game when the window loads
+window.onload = function () {
+  game.initialize();
+};
